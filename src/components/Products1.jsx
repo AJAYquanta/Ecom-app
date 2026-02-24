d2d51748f809aa7e3efc513ffd73540ef85c385f
AJAYquanta <ajay.ass007@gmail.com>
2026-02-24 19:10:10 +0530

    fix: remove invalid Products1 import

--- src/components/Products1.jsx
+++ src/components/Products1.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import mobile from "#";
 import "./Styles.css"; 
 
 const DEFAULT_PRODUCTS = [
