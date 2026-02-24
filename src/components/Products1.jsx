ecd886a14548d737ce000a7b0c8095455f298b26
Ajay _31647 <madalaajaychowdary069@gmail.com>
2026-02-24 18:58:59 +0530

    Products1.jsx

--- src/components/Products1.jsx
+++ src/components/Products1.jsx
@@ -2,20 +2,20 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import mobile from "../images/mobile.png";
+import mobile from "#";
 import "./Styles.css"; 
 
 const DEFAULT_PRODUCTS = [
-  { id: 101, title: "Samsung Galaxy S24 FE", price: 39000, thumbnail: mobile, category: "Electronics" },
-  { id: 102, title: "Samsung Galaxy S25 Ultra", price: 79000, thumbnail: "https://images.samsung.com/ch_fr/smartphones/galaxy-s25-ultra/buy/02_Gallery/02-2_KV_With-Exclusive-Color/02_Online-Exclusive-Single-KV_Titanium_Jetblack_PC.jpg?imbypass=true", category: "Electronics" },
-  { id: 103, title: "Samsung Galaxy Z TriFold", price: 261000, thumbnail: "https://th.bing.com/th/id/OIP.PS-fW9tJj5Oyfb93_1dCvAHaEK?w=295&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3", category: "Electronics" },
-  { id: 104, title: "New Balance classic brount", price: 49999, thumbnail: "https://tse1.mm.bing.net/th/id/OIP.too2O0yo9Jqzl9QNL76M9AHaJT?rs=1&pid=ImgDetMain&o=7&rm=3", category: "Footwear" },
-  { id: 105, title: "New Balance green mounts", price: 38999, thumbnail: "https://sneakernews.com/wp-content/uploads/2023/04/New-Balance-Fresh-Foam-More-Trail-V3-Covert-Green-MTMORNGN-2.jpg", category: "Footwear" },
-  { id: 106, title: "Rolex Submariner", price: 1713902, thumbnail: "https://www.watchclub.com/upload/watches/originali/watch-club-rolex-submariner-date-116613lb-rolexwarrantyto2024-ref-116613lb-year-2019-14676-wb.pngwb.png9.jpg", category: "Watches" },
-  { id: 107, title: "New Balance blue clicks limited Edition", price: 45999, thumbnail: "https://www.newbalance.com.au/dw/image/v2/AASX_PRD/on/demandware.static/-/Library-Sites-NBAU-NBNZ/default/dw900453dd/test-comp-images/2024_Comp_Assets/740-blue-mega-nav.jpg", category: "Footwear" },
-  { id: 108, title: "New Balance art works exp", price: 18999, thumbnail: "https://tse3.mm.bing.net/th/id/OIP.x6658gplZxlS37a8UbJ6UAHaLn?rs=1&pid=ImgDetMain&o=7&rm=3", category: "Footwear" },
-  { id: 109, title: "New Balance new grade fuel cell", price: 14999, thumbnail: "https://th.bing.com/th/id/OIP.zx3s4iG0-kXxSYppwidZFwHaE7?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3", category: "Footwear" },
-  { id: 110, title: "Nike Air Max", price: 13995, thumbnail: "https://tse2.mm.bing.net/th/id/OIP.tAZtpLwas9yqBLsTpL9-5QHaEK?rs=1&pid=ImgDetMain&o=7&rm=3", category: "Footwear" }
+  
+  { id: 101, title: "ipone 16 pro max", price: 10, thumbnail: "https://www.bing.com/th/id/OIP.wGJryIEA2OMCmdQKVPIgtgHaE7?w=273&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2", category: "Electronics" },
+  { id: 102, title: "iphone 17 pro max", price: 20, thumbnail: "https://www.bing.com/th/id/OIP.N0g12LAEm5QWvnNtD2pbJQHaEK?w=245&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2", category: "Electronics" },
+  { id: 103, title: "New Balance classic brount", price: 499, thumbnail: "https://th.bing.com/th/id/OIP.NGhCRhAth0C8blDy8Ns8zgHaEO?w=284&h=180&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3", category: "Footwear" },
+  { id: 104, title: "New Balance green mounts", price: 389, thumbnail: "https://sneakernews.com/wp-content/uploads/2023/04/New-Balance-Fresh-Foam-More-Trail-V3-Covert-Green-MTMORNGN-2.jpg", category: "Footwear" },
+  { id: 105, title: "Rolex Submariner", price: 171, thumbnail: "https://www.watchclub.com/upload/watches/originali/watch-club-rolex-submariner-date-116613lb-rolexwarrantyto2024-ref-116613lb-year-2019-14676-wb.pngwb.png9.jpg", category: "Watches" },
+  { id: 106, title: "New Balance blue clicks limited Edition", price: 459, thumbnail: "https://www.newbalance.com.au/dw/image/v2/AASX_PRD/on/demandware.static/-/Library-Sites-NBAU-NBNZ/default/dw900453dd/test-comp-images/2024_Comp_Assets/740-blue-mega-nav.jpg", category: "Footwear" },
+  { id: 107, title: "New Balance art works exp", price: 18999, thumbnail: "https://tse3.mm.bing.net/th/id/OIP.x6658gplZxlS37a8UbJ6UAHaLn?rs=1&pid=ImgDetMain&o=7&rm=3", category: "Footwear" },
+  { id: 108, title: "New Balance new grade fuel cell", price: 149, thumbnail: "https://th.bing.com/th/id/OIP.zx3s4iG0-kXxSYppwidZFwHaE7?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3", category: "Footwear" },
+  { id: 109, title: "Nike Air Max", price: 139, thumbnail: "https://tse2.mm.bing.net/th/id/OIP.tAZtpLwas9yqBLsTpL9-5QHaEK?rs=1&pid=ImgDetMain&o=7&rm=3", category: "Footwear" }
 ];
 
 function Products1() {
